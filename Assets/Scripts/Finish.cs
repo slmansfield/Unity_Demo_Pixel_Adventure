41993338dc2b32ee11e145bb57b4dccea5c640ee
Shawn Mansfield <shawn.mansfield@gmail.com>
2023-09-13 16:47:32 -0400

    start of end of level code

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField] private AudioSource finishSound;
+
+    
+    void Start()
+    {
+      
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            finishSound.Play();
+        }
+    }
+}
