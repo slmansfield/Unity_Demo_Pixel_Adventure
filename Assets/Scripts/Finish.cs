95430c9626eba24936c763d7ef9bfba4da9817af
Shawn Mansfield <shawn.mansfield@gmail.com>
2023-09-14 07:07:25 -0400

    prepare for creating second level

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class Finish : MonoBehaviour
 {
     [SerializeField] private AudioSource finishSound;
-
+    //private AudioSource finishSound;
+    private bool levelCompleted = false;
     
     void Start()
     {
-      
+        //finishSound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !levelCompleted)
         {
             finishSound.Play();
+            levelCompleted = true;
+            CompleteLevel();
         }
+    }
+
+    private void CompleteLevel()
+    {
+
     }
 }
